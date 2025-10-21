837cca95f97149e93cc2b910f52b813c8fd5e407
Ausaryu <Yonas.houriez@gmail.com>
2025-10-21 08:34:21 +0000

    add 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - ouais
+ *
+ *Return: l'alphabet frère
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
