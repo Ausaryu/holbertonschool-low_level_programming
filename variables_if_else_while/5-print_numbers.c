a3323246c86d6c60c326e168cc876a0e24e2ece5
Ausaryu <Yonas.houriez@gmail.com>
2025-10-21 09:25:18 +0000

    add 5 et 6

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - ouais
+ *
+ *Return: l'alphabet frère
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
