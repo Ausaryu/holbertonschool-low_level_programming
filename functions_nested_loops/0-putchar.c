9f05a85d1c6f066aba6b87f6bbfcb333b45498ad
Ausaryu <Yonas.houriez@gmail.com>
2025-10-21 12:52:48 +0000

    same test but i think i already have a response

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -22,16 +21,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
