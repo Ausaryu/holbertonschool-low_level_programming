ba127dd9e6b4dd820b354d23820b6873a9d40e67
Ausaryu <Yonas.houriez@gmail.com>
2025-10-21 11:59:38 +0000

    add 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - ouais
+ *
+ *Return: l'alphabet frère
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '1'; x <= 'f'; x++)
+	{
+		if (x > '9' && x < 'a')
+		{
+			continue;
+		}
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
