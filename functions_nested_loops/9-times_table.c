77d5d9f428b339c2c0444023a728300128f204aa
Ausaryu <yonas.houriez@gmail.com>
2025-10-22 12:30:12 +0000

    add 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *times_table - tout plein de minutes
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			z = (x * y);
+			if (x == 0)
+			{
+				_putchar('0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			_putchar(',');
+		}
+		putchar('\n');
+	}
+}
