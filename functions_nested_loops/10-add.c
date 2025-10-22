b729b02ef1a5b9b8a714774026738108a7ee90ba
Ausaryu <yonas.houriez@gmail.com>
2025-10-22 12:43:24 +0000

    add 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *add - ouais
+ *@a: one of the int
+ *@b: the second int to add
+ *Return: le result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
