dc6ff824c5a06b3438c7ca32afb03e2413002ad8
Ausaryu <yonas.houriez@gmail.com>
2025-10-28 13:42:43 +0000

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * puts_half - print every other character of a string
+ * @str: the string to print
+ *
+ */
+
+void puts_half(char *str)
+{
+	char *x = str;
+	int count = 0;
+
+	while (*x != '\0')
+	{
+		count++;
+		x++;
+	}
+
+	str = str + ((count + 1) / 2);
+	if (count % 2 != 0)
+		str--;
+
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
