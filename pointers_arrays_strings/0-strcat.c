5da835a9e3daa5ddabc353d16dc446b26c29e2ac
Ausaryu <yonas.houriez@gmail.com>
2025-10-29 11:22:23 +0000

    add 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strcat - append a string to an other
+ * @dest: the base string
+ * @src: the string to append
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	
+	dest[i] = ' ';
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
