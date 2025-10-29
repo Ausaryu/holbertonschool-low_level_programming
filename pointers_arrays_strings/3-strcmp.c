b723ca97e12dd1553c0c8ba40fffdca91deebca4
Ausaryu <yonas.houriez@gmail.com>
2025-10-29 13:06:43 +0000

    add 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strncpy - compare the ascii value of two strings
+ * @s1: string 1 to compare
+ * @s2: string 2 to compare
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+			break;
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
