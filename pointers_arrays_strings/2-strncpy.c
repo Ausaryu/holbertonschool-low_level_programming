ec7d1bbff5595278541ebf98c95f528da2b9deec
Ausaryu <yonas.houriez@gmail.com>
2025-10-29 12:19:48 +0000

    add 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _strncpy - copy a string to an other
+ * @dest: the base string
+ * @src: the string to append
+ * @n: number of characters copied from src
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; j < n; j++)
+	{
+		if (src[j] == '\0')
+		{
+			dest[j] = '\0';
+			break;
+		}
+		dest[j] = src[j];
+	}
+
+
+	return (dest);
+}
