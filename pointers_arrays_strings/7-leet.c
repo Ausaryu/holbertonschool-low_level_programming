a6527b0f3df3fb46262d538b43ff23c12bbbd220
Ausaryu <yonas.houriez@gmail.com>
2025-10-29 16:00:42 +0000

    fix 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,21 +9,20 @@
  */
 char *leet(char *n)
 {
-	int i = 0;
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
-	while (n[i] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (n[i] == 'a' || n[i] == 'A')
-			n[i] = '4';
-		else if (n[i] == 'e' || n[i] == 'E')
-			n[i] = '3';
-		else if (n[i] == 'o' || n[i] == '0')
-			n[i] = '0';
-		else if (n[i] == 't' || n[i] == 'T')
-			n[i] = '7';
-		else if (n[i] == 'l' || n[i] == 'L')
-			n[i] = '1';
-		i++;
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (letters[j] == n[i])
+			{
+				n[i] = numbers[j];
+				break;
+			}
+		}
 	}
 	return (n);
 }
