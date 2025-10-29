999c0c24e7b29446f8d702ffa3142a3dd79f9cdf
Ausaryu <yonas.houriez@gmail.com>
2025-10-29 09:12:33 +0000

    add 100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert string to int
+ * @s: 
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	char *p = s;
+	int sign = 1;
+	int result = 0;
+
+	while (*p < '0' || *p > '9')
+	{
+		if (*p == '\0')
+			break;
+		else if (*p == '-')
+		{
+			sign *= -1;
+			p++;
+		}
+		else
+			p++;
+	}
+
+	while (*p >= '0' && *p <= '9')
+	{
+		result = result * 10 + (*p - '0');
+		p++;
+	}
+	return (sign * result);
+}
