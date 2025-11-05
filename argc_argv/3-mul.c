0b21fe289e27f4fb2ca83c3f31990ceb2228a1c0
Ausaryu <yonas.houriez@gmail.com>
2025-11-05 13:19:01 +0100

    fix betty on 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,19 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b ,mul;
+	int a, b, mul;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[2]);
-    mul = a * b;
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
 
-    printf("%d\n", mul);
+	printf("%d\n", mul);
 
-    return 0;
+	return (0);
 }
