751cef31724f25d0086b3505782f8dac502159ad
Ausaryu <yonas.houriez@gmail.com>
2025-11-10 11:53:29 +0100

    add README main.h 1 and 0 to the maloc free folder

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ *            containing a copy of a given string.
+ * @str: String to duplicate.
+ *
+ * Return: Pointer to duplicated string, or NULL if str is NULL
+ *         or if memory allocation fails.
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int i, size;
+
+	i = 0;
+	size = 0;
+	while (str[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = str[i];
+	}
+	return (array);
+}
