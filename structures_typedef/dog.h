df4e45722a6b3b5966a66f61c2b069d650d277af
Ausaryu <yonas.houriez@gmail.com>
2025-11-12 13:47:06 +0100

    add structures_typedef README and task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents information about an entity.
+ * @name: Name of the entity.
+ * @age: Age of the entity.
+ * @owner: Name of the owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
