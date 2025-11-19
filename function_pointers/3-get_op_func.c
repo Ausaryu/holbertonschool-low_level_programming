c01eeda25192cda971d944daa023981f21caad0f
Ausaryu <yonas.houriez@gmail.com>
2025-11-19 17:39:18 +0100

    fix get op func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,13 +20,15 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	i = 0;
-	while (ops[i].op != NULL)
+	if (s[1] == '\0')
 	{
-		if (*(ops[i].op) == *s)
-			return (ops[i].f);
-		i++;
+		i = 0;
+		while (ops[i].op != NULL)
+		{
+			if (*(ops[i].op) == *s)
+				return (ops[i].f);
+			i++;
+		}
 	}
 	return (NULL);
-
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,15 +20,15 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
 	if (func == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	if ((argv[2][0] == '/' && num2 == 0) ||
 	(argv[2][0] == '%'  && num2 == 0))
