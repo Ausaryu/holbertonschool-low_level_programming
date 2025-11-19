bb5228d13f9d7a3cf9bddb5eb0c3fd7c1b676c9b
Ausaryu <yonas.houriez@gmail.com>
2025-11-19 17:10:34 +0100

    change 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 	int num1, num2;
 	int (*func)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 	if (func == NULL)
